00d2cc07d97bcba8ae268a7f673597a226fe3004
nikola.brkovic <nikola.brkovic@gimb.org>
2021-12-23 11:22:22 +0100

    Add WX socket networking

--- encrypted_chat/encrypted_chat.cpp
+++ encrypted_chat/encrypted_chat.cpp
@@ -3,11 +3,13 @@
 #include "crypto.hpp"
 #include "net.hpp"
 #include <wx/wx.h>
+#include <wx/socket.h>
 
 class User : public CryptographicUser {
 public: 
     std::string name; 
 };
+
 
 User user1;
 User user2;
@@ -15,9 +17,9 @@ User user2;
 class EncryptedChatApp : public wxApp {
 public: 
     EncryptedChatApp();
-    WinsockInterface net_interface;
     virtual ~EncryptedChatApp();
     virtual bool OnInit() override;
+
 };
 
 EncryptedChatApp::EncryptedChatApp()
@@ -31,31 +33,86 @@ EncryptedChatApp::~EncryptedChatApp()
 class AppFrame : public wxFrame
 {
 public:
-    AppFrame(bool status);
+    AppFrame(bool status, wxString hostname);
     wxTextCtrl* text_output;
     wxTextCtrl* text_input;
+    wxSocketServer* sock;
+    wxSocketBase* peer_sock;
+    wxDECLARE_EVENT_TABLE();
 
 private:
     void OnExit(wxCommandEvent& event);
     void UsernamePrompt(wxCommandEvent& event);
     void OnSend(wxCommandEvent& event);
+    void OnServerEvent(wxSocketEvent& event);
+    void OnSocketEvent(wxSocketEvent& event);
+
 };
 
 enum {
     ID_Output = 1,
-    ID_Input = 2
+    ID_Input = 2,
+    ID_Socket = 3,
+    ID_Server = 4,
 };
 
 void AppFrame::OnSend(wxCommandEvent& event) {
     std::string input = text_input->GetValue().utf8_string();
     text_output->WriteText(wxNow() << ' ' << "User 1" << ": " << input << '\n');
-    TransportCipher cipher = user1.encrypt_message(input);
+    /*TransportCipher cipher = user1.encrypt_message(input);
     user2.decrypt_message(cipher);
-    text_output->WriteText(wxNow() << ' ' << "User 2" << ": " << cipher.data << '\n');
+    text_output->WriteText(wxNow() << ' ' << "User 2" << ": " << cipher.data << '\n');*/
+    if (peer_sock) {
+        if (peer_sock->IsConnected()) {
+            peer_sock->Write((input + '\n').c_str(), input.length());
+        }
+    }
     text_input->Clear();
 }
 
-AppFrame::AppFrame(bool status) : wxFrame(nullptr, wxID_ANY, L"Encrypted Chat") {
+void AppFrame::OnServerEvent(wxSocketEvent& event) {
+    switch (event.GetSocketEvent()) {
+    case wxSOCKET_CONNECTION:
+        peer_sock = sock->Accept(false);
+        if (peer_sock) {
+            wxIPV4address addr;
+            if (peer_sock->GetPeer(addr)) {
+                text_output->WriteText(wxNow() << " New connection from " << addr.IPAddress() << '\n');
+            }
+        }
+        peer_sock->SetEventHandler(*this, ID_Socket);
+        peer_sock->SetNotify(wxSOCKET_INPUT_FLAG | wxSOCKET_LOST_FLAG);
+        peer_sock->Notify(true);
+        break;
+    }
+}
+
+void AppFrame::OnSocketEvent(wxSocketEvent& event) {
+    wxSocketBase* sockBase = event.GetSocket();
+    wxUint32 lenRd;
+
+    switch (event.GetSocketEvent()) {
+    case wxSOCKET_INPUT:
+        sockBase->SetNotify(wxSOCKET_LOST_FLAG);
+
+        sockBase->SetFlags(wxSOCKET_WAITALL);
+
+        char buf[16];
+        lenRd = sockBase->Read(buf, 5).LastCount();
+
+        if (lenRd) {
+            text_output->WriteText(wxNow() << ' ' << "User 2: " << buf);
+        }
+
+        sockBase->SetNotify(wxSOCKET_LOST_FLAG | wxSOCKET_INPUT_FLAG);
+        break;
+    case wxSOCKET_LOST:
+        sockBase->Destroy();
+        break;
+    }
+}
+
+AppFrame::AppFrame(bool status, wxString hostname) : wxFrame(nullptr, wxID_ANY, L"Encrypted Chat") {
 
     wxBoxSizer* vbox = new wxBoxSizer(wxVERTICAL);
     wxPanel* panel = new wxPanel(this, wxID_ANY);
@@ -76,6 +133,20 @@ AppFrame::AppFrame(bool status) : wxFrame(nullptr, wxID_ANY, L"Encrypted Chat") 
     else
         SetStatusText("Failed to generate keypair!");
 
+    wxIPV4address addr;
+
+    addr.AnyAddress();
+    if (hostname != "localhost")
+        addr.Hostname(hostname);
+
+    addr.Service(28015);
+    
+    sock = new wxSocketServer(addr);
+
+    sock->SetEventHandler(*this, ID_Server);
+    sock->SetNotify(wxSOCKET_CONNECTION_FLAG);
+    sock->Notify(true);
+
     Bind(wxEVT_TEXT_ENTER, &AppFrame::OnSend, this, ID_Input);
     Center();
 }
@@ -93,15 +164,24 @@ bool EncryptedChatApp::OnInit()
     user1.name = "me";
     user2.name = "you";
 
-    net_interface.Receive();
-    if (net_interface.iResult != 0) {
-        std::cout << "Failed net init" << std::endl;
+    wxString addr;
+    if (wxApp::argc > 1) {
+        addr = wxApp::argv[0];
     }
+    else {
+        addr = "localhost";
+    }
+    AppFrame* mainFrame = new AppFrame(key_exchange(user1, user2), addr);
 
-    AppFrame* mainFrame = new AppFrame(key_exchange(user1, user2));
     mainFrame->Show(true);
+
     return true;
 }
+
+wxBEGIN_EVENT_TABLE(AppFrame, wxFrame)
+EVT_SOCKET(ID_Server, AppFrame::OnServerEvent)
+EVT_SOCKET(ID_Socket, AppFrame::OnSocketEvent)
+wxEND_EVENT_TABLE()
 
 wxIMPLEMENT_APP(EncryptedChatApp);
 
